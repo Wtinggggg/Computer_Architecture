f06f82875849de0e99df4bc02b811dfb348d8136
Wtinggggg <tingwei2805@gmail.com>
2024-10-12 14:39:06 +0800

    Upload testing data

--- HW1/TestCode/Max_subarray_test.cpp
+++ HW1/TestCode/Max_subarray_test.cpp
@@ -16,9 +16,18 @@ int maxSubArray(int* nums, int numsSize) {
 }
 
 int main() {
-    int nums[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-    int numsSize = sizeof(nums) / sizeof(nums[0]);
-    int max_sum = maxSubArray(nums, numsSize);
-    printf("Maximum subarray sum is %d\n", max_sum);
+    
+    int nums_1[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int nums_2[] = {1};
+    int nums_3[] = {5,4,-1,7,8};
+    int numsSize_1 = sizeof(nums_1) / sizeof(nums_1[0]);
+    int numsSize_2 = sizeof(nums_2) / sizeof(nums_2[0]);
+    int numsSize_3 = sizeof(nums_3) / sizeof(nums_3[0]);
+    int max_sum_1 = maxSubArray(nums_1, numsSize_1);
+    int max_sum_2 = maxSubArray(nums_2, numsSize_2);
+    int max_sum_3 = maxSubArray(nums_3, numsSize_3);
+    printf("Maximum subarray sum is %d\n", max_sum_1);
+    printf("Maximum subarray sum is %d\n", max_sum_2);
+    printf("Maximum subarray sum is %d\n", max_sum_3);
     return 0;
 }
